e82e90b170d8a97c65a0282d754a993e7db1c406
Improven <98059527+ImprovenHub@users.noreply.github.com>
2025-02-25 16:54:44 +0100

    Update CalcSklum(2).py

--- CalcSklum(2).py
+++ CalcSklum(2).py
@@ -68,7 +68,7 @@ def eliminar_todas_las_tablas():
     conn.commit()
     conn.close()
 
-#eliminar_todas_las_tablas()
+eliminar_todas_las_tablas()
 
 def vaciar_bd_retribuciones2():
     conn = sqlite3.connect('retribuciones4.db')
